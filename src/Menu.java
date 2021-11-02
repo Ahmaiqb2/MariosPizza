770e7c952d4fa5f112cba553e84fcee14d29db2f
abdu0939 <navn3466@stud.kea.dk>
2021-11-03 00:55:43 +0100

    nye pizzaer i menukort

--- src/Menu.java
+++ src/Menu.java
@@ -43,15 +43,15 @@ public class Menu {
     public void menuIndhold() {
         pizza = new ArrayList<>();
         Pizza pizza1 = new Pizza(1, "Vesuvio:",
-                "tomatsauce, ost, skinke og oregano", 57.0);
+                "tomatsauce, ost, skinke og oregano", 57);
         Pizza pizza2 = new Pizza(2, "Amerikaner",
                 "tomatsauce, ost, oksefars og oregano", 53);
         Pizza pizza3 = new Pizza(3, "Casiatore",
                 " tomatsauce, ost, pepperoni og oregano", 57);
         Pizza pizza4 = new Pizza(4, "Carbonara",
-                "tomatsauce, ost, kodsauce, spaghetti, cocktailpølser og oregano", 63);
+                " tomatsauce, ost, kodsauce, spaghetti, cocktailpølser og oregano", 63);
         Pizza pizza5 = new Pizza(5, "Dennis",
-                "tomaatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", 65);
+                " tomaatsauce , ost, skinke, pepperoni, cocktailpølser og oregano", 65);
         Pizza pizza6 = new Pizza(6, "Bertil",
                 "tomatsauce, ost, bacon, oregano", 57);
         Pizza pizza7 = new Pizza(7, "Silvia",
@@ -68,8 +68,41 @@ public class Menu {
                 "tomatsauce, ost, skinke, rejer og oregano", 61);
         Pizza pizza13 = new Pizza(13, "Venezia",
                 "tomatsauce, ost, skinke, bacon og oregano", 61);
-        Pizza pizza14 = new Pizza(14, "Mafia",
-                "tomatsauce, ost, pepperoni, bacon, løg, oregano", 61);
+        Pizza pizza14 = new Pizza(14, "Nørrebro",
+                "tomatsauce, ost, kebab, champingon, salat, tomat og dressing, ", 61);
+        Pizza pizza15 = new Pizza(15, "Mike Tyson XL",
+                "tomatsauce, ost, kylling, kebab, dressing, iceberg og tomat", 85);
+        Pizza pizza16 = new Pizza(16, "Roma",
+                "tomatsauce, ost, oregano og prosciutto", 61);
+        Pizza pizza17 = new Pizza(17, "Mette Frederiksen",
+                "tomatsauce, ost, basilikum og makrel", 61);
+        Pizza pizza18 = new Pizza(18, "White truffle",
+                "Mornaysauce, trøffelolie, pesto og hvidløg", 85);
+        Pizza pizza19 = new Pizza(19, "Vegan",
+                " tomatsauce, veganCheez, likeMeat og oregano", 57);
+        Pizza pizza20 = new Pizza(20, "Holger Danske",
+                "tomatsauce, ost, bacon, bernaisesauce og champingion", 63);
+        Pizza pizza21 = new Pizza(21, "Thyborøn XL",
+                "tomatsauce, ost, rejer, tun, ansjoser og  basilikum", 85);
+        Pizza pizza22 = new Pizza(22, "Butter Chiken",
+                "Butterchikensauce, ost, persille og hvidløg", 57);
+        Pizza pizza23 = new Pizza(23, "Sinbad",
+                "tomatsauce, ost, pepperoni, rød peber, løg, oliven, kebab og aubergine", 75);
+        Pizza pizza24 = new Pizza(24, "Marios",
+                "tomatsauce, ost, rødløg og oregano", 61);
+        Pizza pizza25 = new Pizza(25, "Alfonso",
+                "tomatsauce, ost, skinke, bacon, chili og oregano", 61);
+        Pizza pizza26 = new Pizza(26, "After Eight",
+                "tomatsauce, ost, skinke, bacon, kebab, champignon og oregano", 61);
+        Pizza pizza27 = new Pizza(27, "study time",
+                "tomatsauce, ost,kylling og oregano", 61);
+        Pizza pizza28 = new Pizza(28, "Bella Mia",
+                "tomatsauce, ost, skinke, rejer og oregano", 61);
+        Pizza pizza29 = new Pizza(29, "Mamma mia XL",
+                "kødsauce, ost, skinke, rødløg og peber", 85);
+        Pizza pizza30 = new Pizza(30, "Santa Maria",
+                "texmexsauce, ost, pepperoni, kylling, løg og  oregano", 61);
+
 
         pizza.add(pizza1);
         pizza.add(pizza2);
@@ -85,7 +118,24 @@ public class Menu {
         pizza.add(pizza12);
         pizza.add(pizza13);
         pizza.add(pizza14);
+        pizza.add(pizza15);
+        pizza.add(pizza16);
+        pizza.add(pizza17);
+        pizza.add(pizza18);
+        pizza.add(pizza19);
+        pizza.add(pizza20);
+        pizza.add(pizza21);
+        pizza.add(pizza22);
+        pizza.add(pizza23);
+        pizza.add(pizza24);
+        pizza.add(pizza25);
+        pizza.add(pizza26);
+        pizza.add(pizza27);
+        pizza.add(pizza28);
+        pizza.add(pizza29);
+        pizza.add(pizza30);
     }
+
 
     /*public void printMenu(){
         for (int i = 0; i < pizza.size(); i++){
