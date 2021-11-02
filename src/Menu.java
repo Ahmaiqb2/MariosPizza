d399cf2606fd4e6496cf7f501474cf3c802b5d95
Ahmaiqb <Ahsa0084@stud.kea.dk>
2021-11-02 13:41:37 +0100

    Menu pizza

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+
 public class Menu {
 
-    public Menu(){
+    private ArrayList<Pizza> pizza;
 
+    public Menu(){
+        printMenu();
+    }
+
+    public void printMenu(){
+        pizza = new ArrayList<>();
+        Pizza pizza1 = new Pizza(1,"Vesuvio:",
+                "tomatsause, ost, skinke, oregano",57);
+
+        pizza.add(pizza1);
+    }
+
+    public void printeUd(){
+        for (int i = 0; i < pizza.size(); i++){
+            System.out.println(pizza.get(i));
+        }
+    }
+
+    public ArrayList<Pizza> getPizza() {
+        return pizza;
     }
 }
--- src/PizzaBar.java
+++ src/PizzaBar.java
@@ -4,6 +4,7 @@ public class PizzaBar {
 
     private Scanner sc = new Scanner(System.in);
     private UserInterface ui = new UserInterface();
+    private Menu menu = new Menu();
 
     public void run() {
         boolean startGo = true;
@@ -16,15 +17,24 @@ public class PizzaBar {
             switch (input) {
 
                 case "1":
-
+                    menu.printeUd();
+                    break;
                 case "2":
+
+                    break;
 
                 case "3":
 
+                    break;
+
                 case "4":
+
+                    break;
 
                 case "9":
                     ui.farvelBesked();
+
+                    break;
 
                 default:
                     System.out.println("Dette er ikke en mulighed");
